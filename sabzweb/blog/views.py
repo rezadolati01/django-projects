67740515b148473df9224eaaa4165a944455fb56
reza dolati <reza.75twd@yahoo.com>
2023-05-11 02:55:56 +0430

    pagination

--- sabzweb/blog/views.py
+++ sabzweb/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
@@ -12,6 +13,14 @@ def index(request):
 
 def post_list(request):
     posts = Post.published.all()
+    paginator = Paginator(posts, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
     context = {
         'posts': posts,
     }
