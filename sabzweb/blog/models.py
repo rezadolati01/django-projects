42af9c760fc47ca786d4ea3ffa05b9d7dc263406
reza dolati <reza.75twd@yahoo.com>
2023-05-10 22:38:34 +0430

    template style

--- sabzweb/blog/models.py
+++ sabzweb/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django_jalali.db import models as jmodels
+from django.urls import reverse
 
 # Managers
 class PublishedManager(models.Manager):
@@ -43,3 +44,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.id])
+
