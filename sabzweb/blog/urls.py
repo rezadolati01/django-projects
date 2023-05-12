cae41ac4e0871f97336d430fa2fa2a25dc1b7e44
reza dolati <reza.75twd@yahoo.com>
2023-05-12 20:53:59 +0330

    class based views

--- sabzweb/blog/urls.py
+++ sabzweb/blog/urls.py
@@ -5,6 +5,8 @@ app_name = "blog"
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('posts/', views.post_list, name="post_list"),
-    path('posts/<int:id>', views.post_detail, name="post_detail"),
+    # path('posts/', views.post_list, name="post_list"),
+    path('posts/', views.PostListView.as_view(), name="post_list"),
+    # path('posts/<int:id>', views.post_detail, name="post_detail"),
+    path('posts/<pk>', views.PostDetailView.as_view(), name="post_detail"),
 ]
--- sabzweb/blog/views.py
+++ sabzweb/blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from .models import Post
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView, DetailView
 
 
 # Create your views here.
@@ -11,25 +12,37 @@ def index(request):
     return HttpResponse("index")
 
 
-def post_list(request):
-    posts = Post.published.all()
-    paginator = Paginator(posts, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    context = {
-        'posts': posts,
-    }
-    return render(request, "blog/list.html", context)
+# def post_list(request):
+#     posts = Post.published.all()
+#     print(posts, type(posts))
+#     paginator = Paginator(posts, 2)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     print(posts, type(posts))
+#     context = {
+#         'posts': posts,
+#     }
+#     return render(request, "blog/list.html", context)
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/list.html"
 
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
-    context = {
-        'post': post,
-    }
-    return render(request, "blog/detail.html", context)
+# def post_detail(request, id):
+#     post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+#     context = {
+#         'post': post,
+#     }
+#     return render(request, "blog/detail.html", context)
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/detail.html"
