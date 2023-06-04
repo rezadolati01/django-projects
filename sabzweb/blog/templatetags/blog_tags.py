09ce6ef01146a0fbea3d028673a398e393bc5e4a
reza dolati <reza.75twd@yahoo.com>
2023-06-04 13:01:59 +0330

    inclusion_tag

--- sabzweb/blog/templatetags/blog_tags.py
+++ sabzweb/blog/templatetags/blog_tags.py
@@ -17,3 +17,12 @@ def total_comments():
 @register.simple_tag()
 def last_post_date():
     return Post.published.last().publish
+
+
+@register.inclusion_tag("partials/latest_posts.html")
+def latest_posts(count=4):
+    l_posts = Post.published.order_by('-publish')[:count]
+    context = {
+        'l_posts': l_posts
+    }
+    return context
