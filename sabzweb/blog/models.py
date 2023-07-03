add1e2e6dfea90fa9c10ba4411abf989a8691c65
reza dolati <reza.75twd@yahoo.com>
2023-07-03 13:50:25 +0330

    django-resized

--- sabzweb/blog/models.py
+++ sabzweb/blog/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django_jalali.db import models as jmodels
 from django.urls import reverse
+from django_resized import ResizedImageField
 
 
 # Managers
@@ -88,7 +89,7 @@ class Comment(models.Model):
 
 class Image(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="images", verbose_name="پست")
-    image_file = models.ImageField(upload_to="post_images/")
+    image_file = ResizedImageField(upload_to="post_images/", size=[500, 500], quality=100, crop=['middle', 'center'])
 
     title = models.CharField(max_length=250, verbose_name="عنوان", null=True, blank=True)
     description = models.TextField(verbose_name="توضیحات", null=True, blank=True)
