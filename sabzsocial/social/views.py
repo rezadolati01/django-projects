3eea54bba758566322a837e8fb7440d77f75ab54
reza dolati <reza.75twd@yahoo.com>
2023-10-16 18:26:44 +0330

    user list and user detail

--- sabzsocial/social/views.py
+++ sabzsocial/social/views.py
@@ -165,3 +165,14 @@ def save_post(request):
 
         return JsonResponse({'saved': saved})
     return JsonResponse({'error': 'Invalid request'})
+
+
+@login_required
+def user_list(request):
+     users = User.objects.filter(is_active=True)
+     return render(request, 'user/user_list.html', {'users': users})
+
+@login_required
+def user_detail(request, username):
+     user = get_object_or_404(User, username=username, is_active=True)
+     return render(request, 'user/user_detail.html', {'user': user})
