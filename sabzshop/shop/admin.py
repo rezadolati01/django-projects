7cc4136f683f3eb203a6d3fb070b747285ed3f4d
reza dolati <reza.75twd@yahoo.com>
2023-12-05 13:01:40 +0330

    admin

--- sabzshop/shop/admin.py
+++ sabzshop/shop/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 0
+
+
+class FeatureInline(admin.TabularInline):
+    model = ProductFeature
+    extra = 0
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'inventory', 'new_price', 'created', 'updated']
+    prepopulated_fields = {'slug': ('name',)}
+    list_filter = ['created', 'updated']
+    inlines = [ImageInline, FeatureInline]
--- sabzshop/shop/apps.py
+++ sabzshop/shop/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ShopConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'shop'
+
+    def ready(self):
+        import shop.signals
