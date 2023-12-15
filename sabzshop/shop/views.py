a715faf41df2c95af4186a1d5c60b72825026cf6
reza dolati <reza.75twd@yahoo.com>
2023-12-15 18:23:45 +0330

    product list template

--- sabzshop/shop/views.py
+++ sabzshop/shop/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
+
 
 # Create your views here.
+
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    context = {
+        'category': category,
+        'categories': categories,
+        'products': products
+    }
+    return render(request, 'shop/list.html', context)
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, slug=slug, id=id)
+    context = {
+        'product': product,
+    }
+    return render(request, 'shop/detail.html', context)
