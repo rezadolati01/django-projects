05b963f4f040da69068910735c29ac21556423e7
reza dolati <reza.75twd@yahoo.com>
2023-12-18 16:33:24 +0330

    add to cart ajax

---
+++ sabzshop/cart/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'cart'
+
+urlpatterns = [
+    path('add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+]
--- sabzshop/cart/views.py
+++ sabzshop/cart/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_POST
+from shop.models import Product
+from .cart import Cart
+from django.http import JsonResponse
+
 
 # Create your views here.
+
+@require_POST
+def add_to_cart(request, product_id):
+    try:
+        cart = Cart(request)
+        product = get_object_or_404(Product, id=product_id)
+        cart.add(product)
+        cart.save()
+        context = {
+            'item_count': len(cart),
+            'total_price': cart.get_total_price(),
+        }
+        return JsonResponse(context)
+
+    except:
+        return JsonResponse({"error": "Invalid request."})
