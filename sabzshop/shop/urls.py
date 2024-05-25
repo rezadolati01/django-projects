741f72b96aebd7d99ca271224fae2110f154f30f
Amin Rastgari <rastgarii.amin@gmail.com>
2024-05-25 23:07:02 +0300

    update templates

--- sabzshop/shop/urls.py
+++ sabzshop/shop/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'shop'
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('products/', views.product_list, name='product_list'),
     path('products/<slug:category_slug>/', views.product_list, name='product_list_by_category'),
     path('product/<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
